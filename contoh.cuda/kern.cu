bf995929e6f3b852ff6eb5195c038d4aea61023e
wijonur25 <56059022+wijonur25@users.noreply.github.com>
2019-10-03 11:57:33 +0700

    Update kern.cu

--- contoh.cuda/kern.cu
+++ contoh.cuda/kern.cu
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
-
+#define N 12
 // tugas 1: alokasi memori dan transfer dari device ke host
 
 __global__ void
 kern(int *A)
 {
-    	int idx = blockDim.x * blockIdx.x + threadIdx.x;
- 	A[idx] = idx;
+        int idx = blockDim.x * blockIdx.x + threadIdx.x;
+        A[idx] = idx;
 }
 
 /**
@@ -15,11 +15,21 @@ kern(int *A)
  */
 int   main(void)
 {
-	// alokasikan memori, dan salin nilainya
-	dim3 grid,block;
-	block.x = 4;
-	grid.x = 12/block.x;
-	//kern<<grid,block>>(d_A);
-	// copy result
-	return 0;
+        // alokasikan memori, dan salin nilainya
+        int *A = (int *) malloc(N*sizeof(int)); //alokasi memori di host
+        int *d_A;
+        cudaMalloc(&d_A,N*sizeof(int)); //alokasi memori di device
+        cudaMemcpy(d_A,A,N*sizeof(int),cudaMemcpyHostToDevice);
+
+        dim3 grid,block;
+        block.x = 4;
+        grid.x = 12/block.x;
+        kern<<<grid,block>>>(d_A);
+        cudaMemcpy(A,d_A,N*sizeof(int),cudaMemcpyDeviceToHost);
+        // copy result
+        for(int i = 0;i < N;i++)
+                printf("A[%d] = %d\n",i,A[i]);
+        free(A);
+        cudaFree(d_A);
+        return 0;
 }
